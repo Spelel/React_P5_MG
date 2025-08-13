114476ba27dcd0c8d0ab89339061b46b2bd94d3e
Arek <arek-karczo.wp.pl@wp.pl>
2025-08-13 20:11:19 +0200

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Heder from './components/Heder'
 
 function App() {
-  
 
-  return (
-    <>
-      
+  return (<>
+      <Heder />
     </>
   )
 }
---
+++ src/components/Heder.jsx
@@ -0,0 +1,10 @@
+import tf from '../assets/troll-face.png'
+export default function Heder() {
+    
+    return<>
+        <heder className='' >
+            <img src={tf} />
+            <h1>Meme Generator</h1>
+        </heder>
+    </>
+}
